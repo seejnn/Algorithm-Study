80bc5e342b0ad719876a49057c3837fee744474d
llamazon <156206689+llamazon@users.noreply.github.com>
2024-01-11 02:35:11 +0900

    [level unrated] Title: 문자열 곱하기, Time: 3.46 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/llamazon-문자열 곱하기.js
+++ 프로그래머스/unrated/181940. 문자열 곱하기/llamazon-문자열 곱하기.js
@@ -1,1 +1,5 @@
-const solution = (my_string, k) => my_string.repeat(k);
+function solution(my_string, k) {
+    var answer = '';
+    console.log(Array.from(Array(k).keys()))
+    return Array.from(Array(k).keys()).map((el) => my_string).join('');
+}
