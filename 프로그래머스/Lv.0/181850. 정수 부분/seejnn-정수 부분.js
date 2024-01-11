204fe8b2e69942d3e5218389949e96783dce282a
seejnn <44645578+seejnn@users.noreply.github.com>
2024-01-11 11:37:15 +0900

    [level Lv.0] Title: 정수 부분, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181850. 정수 부분/seejnn-정수 부분.js
@@ -0,0 +1,8 @@
+function solution(flo) {
+    var answer = 0;
+    
+    // answer = flo.split('.')[0]
+    // answer = flo.split(.)[0]
+    answer = Number(String(flo).split('.')[0])
+    return answer;
+}
