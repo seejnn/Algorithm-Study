e5952167820e44222e8651be41da66aa6cb33676
seejnn <44645578+seejnn@users.noreply.github.com>
2024-01-11 17:20:10 +0900

    Update stats.py

--- stats.py
+++ stats.py
@@ -44,28 +44,61 @@ def update_readme(token, user, repo, top_committer, commits):
     g = Github(token)
     repo = g.get_repo(f"{user}/{repo}")
 
-    # Update README
-    readme_content = f"
+#     # Update README
+#     readme_content = f"
 
-## 사용법
-1. BaekjoonHub 크롬 확장 프로그램 설치 (@seejnn 에게 문의주세요)
-2. Github 계정 연동
-3. 알고리즘 스터디 repository 연동
+# ## 사용법
+# 1. BaekjoonHub 크롬 확장 프로그램 설치 (@seejnn 에게 문의주세요)
+# 2. Github 계정 연동
+# 3. 알고리즘 스터디 repository 연동
 
-위의 과정을 마친 후 
+# 위의 과정을 마친 후 
 
-백준 / 프로그래머스 / SWEA 에서 문제를 풀면 자동으로
+# 백준 / 프로그래머스 / SWEA 에서 문제를 풀면 자동으로
 
-해당 문제 정보와 작성한 코드가 레포지토리에 업로드됩니다.
+# 해당 문제 정보와 작성한 코드가 레포지토리에 업로드됩니다.
 
-<br />
+# <br />
 
-현재는 BaekjoonHub의 기본 설정인 난이도별로 디렉토리가 설정됩니다.
+# 현재는 BaekjoonHub의 기본 설정인 난이도별로 디렉토리가 설정됩니다.
 
 
-# 커밋 순위 \n\n- 1등: {top_committer} - {commits} 커밋\n"
+# # 커밋 순위 \n\n- 1등: {top_committer} - {commits} 커밋\n"
+#     readme_file = repo.get_contents("README.md", ref="main")
+#     repo.update_file(readme_file.path, "Update top committer stats", readme_content, readme_file.sha)
+
+# # GitHub Personal Access Token
+# token = os.getenv('GITHUB_TOKEN')
+
+# user = "seejnn"
+# repo = "Algorithm-Study"
+# contributors = fetch_commit_data(token, user, repo)
+# top_committer, commits = extract_top_committer(contributors)
+# update_readme(token, user, repo, top_committer, commits)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Fetching current README content
     readme_file = repo.get_contents("README.md", ref="main")
-    repo.update_file(readme_file.path, "Update top committer stats", readme_content, readme_file.sha)
+    current_content = readme_file.decoded_content.decode("utf-8")
+
+    # Appending the top committer information
+    updated_content = current_content + f"\n- 1등: {top_committer} - {commits} 커밋\n"
+
+    # Update README
+    repo.update_file(readme_file.path, "Update top committer stats", updated_content, readme_file.sha)
 
 # GitHub Personal Access Token
 token = os.getenv('GITHUB_TOKEN')
@@ -75,4 +108,3 @@ repo = "Algorithm-Study"
 contributors = fetch_commit_data(token, user, repo)
 top_committer, commits = extract_top_committer(contributors)
 update_readme(token, user, repo, top_committer, commits)
-
